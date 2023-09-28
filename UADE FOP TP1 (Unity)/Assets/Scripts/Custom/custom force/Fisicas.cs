35ad91f6a992a55dc9c92fa2c8b9c6ca2a7f6338
hernancastro1234 <114106799+hernancastro1234@users.noreply.github.com>
2023-09-28 20:15:46 -0300

    Mejoras en General

---
+++ UADE FOP TP1 (Unity)/Assets/Scripts/Custom/custom force/Fisicas.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class Fisicas : MonoBehaviour
+{
+   
+    public float mass = 1;
+    public Vector2 velocity;
+    //public int a�adirFuerza;
+
+    public float gravityScale = 0.0f;
+
+    private Vector2 gravity = new Vector2(0, -5.8f); // Ajusta la gravedad seg�n tus necesidades
+
+    void FixedUpdate()
+    {
+        // Update the position based on the velocity
+        transform.position += (Vector3)velocity * Time.fixedDeltaTime;        
+    }   
+
+    public void CustomAddForce(Vector2 force)
+    {
+        // Calculate acceleration using F = ma
+        Vector2 acceleration = force / mass;
+        // Update velocity using the calculated acceleration
+        velocity += acceleration * Time.fixedDeltaTime;
+        
+    }
+    public void CustomAddImpulse(Vector2 impulse)
+    {
+        // Update velocity using the impulse
+        velocity += impulse / mass;
+    }
+
+    public void Stop()
+    {
+        velocity = Vector2.zero;
+    }
+}
--- UADE FOP TP1 (Unity)/Assets/Scripts/Manager/GameManager.cs
+++ UADE FOP TP1 (Unity)/Assets/Scripts/Manager/GameManager.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Pool _pool;
+    [SerializeField] private List<CustomMonoBehaviour> _customMonoBehaviours = new();
+
+    public List<CustomMonoBehaviour> CustomMonoBehaviours => _customMonoBehaviours;
+
+    // Game Manager Instance
+    public static GameManager Instance;
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CheckCollisions(CustomColliderBase myCollider)
     {
-        
+        for (int i = CustomMonoBehaviours.Count - 1; i >= 0; i--)
+        {            
+            var otherObject = CustomMonoBehaviours[i];
+            if (otherObject == myCollider.gameObject) continue;
+
+            if (otherObject.isActiveAndEnabled)
+            {
+                if (myCollider.CheckCollision(otherObject.CustomCollider));
+            }
+        }
     }
 }
